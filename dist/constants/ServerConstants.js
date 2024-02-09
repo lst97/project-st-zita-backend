161c1112ae5bd4cf96a935f5136120df8802d483
lst97 <laisiotou1997@gmail.com>
2024-02-09 16:59:06 +1100

    update: implement basic login function

--- dist/constants/ServerConstants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_BASE_URL = exports.API_ENDPOINT = exports.HOST = exports.PORT = exports.API_VERSION = void 0;
-exports.API_VERSION = "v1";
-exports.PORT = 1168;
-exports.HOST = "localhost";
-exports.API_ENDPOINT = `/api/${exports.API_VERSION}`;
-exports.API_BASE_URL = `http://${exports.HOST}:${exports.PORT}${exports.API_ENDPOINT}`;
